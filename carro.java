cae7b131283e9fc6fe8e01a5e1b6bc76df3de3e7
Duda Melim <137728498+dudelim@users.noreply.github.com>
2023-08-09 15:57:13 -0300

    carro.java

---
+++ carro.java
@@ -0,0 +1,16 @@
+public class Carro {
+  int ano;
+  String marca;
+  String modelo;
+  String cor;
+
+  public void exibirModelo() {
+    System.out.println("Modelo: " + modelo);
+  }
+
+  public void exibirAnoECor() {
+    System.out.println("Ano: " + ano);
+    System.out.println("Cor: " + cor);
+  }
+
+}
